7772da3be582c79f0eb49825941523d48d79a4de
Josue Mapelli <kxel@outlook.com>
2018-05-26 10:17:15 -0500

    fix ignore

---
+++ api/controllers/junta.controller.js
@@ -0,0 +1,12 @@
+exports.findByUsuario = (req, res) =>{
+    res.json({"message": "Find by usuario "+req.params.id});
+}
+
+exports.findOne = (req, res) =>{
+    res.json({"message": "Find one "+req.params.id});
+}
+
+exports.create = (req, res) =>{
+    res.json({"message": "Create"});
+}
+
