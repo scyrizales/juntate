a7d22ae7c673ecdaa5d204eab118e4da70f4fac9
Emmanuel Barturen <emmanuelbarturen@gmail.com>
2018-05-26 13:54:38 -0500

    template

--- web/juntate-fe/src/app/app.module.ts
+++ web/juntate-fe/src/app/app.module.ts
@@ -1,23 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
+import {AppComponent} from './app.component';
+import {RegistrationFormComponent} from './components/registration-form/registration-form.component';
+import {RegistrationServiceService} from './services/registration-service.service';
+import {IndexComponent} from './components/index/index.component';
 
-import { AppComponent } from './app.component';
-import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
-import { RegistrationServiceService } from './services/registration-service.service';
-
+const appRoutes: Routes = [
+    {path: '', component: IndexComponent},
+    {path: 'aperturar', component: RegistrationFormComponent},
+    /*{
+        path: 'heroes',
+        component: HeroListComponent,
+        data: { title: 'Heroes List' }
+    },
+    { path: '',
+        redirectTo: '/heroes',
+        pathMatch: 'full'
+    },
+    { path: '**', component: PageNotFoundComponent }*/
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegistrationFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [ RegistrationServiceService ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RegistrationFormComponent,
+        IndexComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot(
+            appRoutes,
+            {enableTracing: true} // <-- debugging purposes only
+        )
+    ],
+    providers: [RegistrationServiceService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
