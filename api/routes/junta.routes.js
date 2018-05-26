90d4d6c98b64d428daf0e14b729a62c0002a3c2b
scyrizales <scyrizales@gmail.com>
2018-05-26 13:00:41 -0500

    Merge

--- api/routes/junta.routes.js
+++ api/routes/junta.routes.js
@@ -3,10 +3,6 @@ module.exports = (app) => {
 
     app.get('/junta/findAll', juntaController.findAll);
     app.get('/junta/findOne/:id', juntaController.findOne);
-<<<<<<< HEAD
-    app.get('/junta/create', juntaController.create)
-=======
     app.post('/junta/create', juntaController.create)
->>>>>>> 1477b38d4952b1159bd24a3461adcb9b8f16496e
     
 }
